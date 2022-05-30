41ce18bd844a8db4e880b9472de2e31fb491f98b
tsubasanagatsuka <reach.283.0921@gmail.com>
2022-05-30 15:12:41 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,13 @@
+class Prototype < ApplicationRecord
+
+  validates :image,      presence: true
+  validates :title,      presence: true
+  validates :catch_copy, presence: true
+  validates :concept,    presence: true
+  
+
+  belongs_to :user
+  has_many :comments,dependent: :destroy
+
+  has_one_attached :image
+end
